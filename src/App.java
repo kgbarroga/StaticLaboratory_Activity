ddbd9bc1ebd03cc35d85fd4510ce9701ed9e954a
marco <kmgbarroga@gmail.com>
2022-04-04 14:38:54 +0800

    init-commit

---
+++ src/App.java
@@ -0,0 +1,23 @@
+public class App {
+    /**
+     * Complete the Program
+     * 
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("Howdy Coder!");
+        /**
+         * Model A Player Class with A Name(string), and Level(int)
+         */
+
+        /**
+         * Create 2-5 Objects with the Player Class, and initialize
+         * the values via the constructor.
+         * eg.
+         * Player one = new Player("Marco",100)
+         */
+
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,44 @@
+public class Player {
+    /**
+     * Instance Variable Requirements
+     * Name
+     * Level
+     *
+     */
+    private String name;
+
+    /**
+     * Static Variable Requirement
+     * Create a static Variable called max_level(int)
+     * It should hold the value of the Max Level of the players created.
+     */
+
+
+    /**
+     * Create static variable called player_count(int)
+     * It should hold the value of 
+     */
+
+
+    Player(String playerName,int playerLevel){
+        /**
+         * Hint: 
+         * Perform comparison here.
+         * Update the counter here.
+         */
+
+    }
+    /**
+     * Create getters for max_level, and counter
+     * @return max_level
+     * 
+     */
+
+    /*
+    Example
+    public static int getMaxLevel(){
+        return max_level
+    }
+    */
+    
+}
